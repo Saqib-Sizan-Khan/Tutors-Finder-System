56bfdf302e4fbddd2347e798b673e3c13acfa702
Jannatul Ferdous Eva <jannatuleva990@gmail.com>
2021-04-04 20:20:03 +0600

    Eva update 2

---
+++ educator/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class HomeEducator(models.Model):
+#     homeTutorName = models.TextField(db_column='Tutor Name')
+#     homeTutorID = models.IntegerField(primary_key=True, db_column='Tutor Id')
+#     homeTutorEmail = models.EmailField(max_length=50, db_column='Email')
+#     university = models.TextField(db_column='University')
+#     deparment = models.TextField(db_column='Deparment')
+#     experience = models.CharField(max_length=20, db_column='Experience')
+#     homeTutorAddress = models.TextField(db_column='Address')
+#     homeTutorContact = models.CharField(max_length=11, db_column='Contact')
+#     homeTutorRating = models.IntegerField(db_column='Rating')
+#
+#     class Meta:
+#         db_table = 'Home Tutor Info'
+#
+# class OutsideEducator(models.Model):
+#     outsideTutorName = models.TextField(db_column='Tutor Name')
+#     outsideTutorID = models.IntegerField(primary_key=True, db_column='Tutor Id')
+#     outsideTutorEmail = models.EmailField(max_length=50, db_column='Email')
+#     institute = models.TextField(db_column='Institute')
+#     designation = models.TextField(db_column='Designation')
+#     outsideTutorAddress = models.TextField(db_column='Address')
+#     outsideTutorContact = models.CharField(max_length=11, db_column='Contact')
+#     outsideTutorRating = models.IntegerField(db_column='Rating')
+#
+#     class Meta:
+#         db_table = 'Outside Tutor Info'
+
+
+class HomeEducatorSubjects(models.Model):
+    firstSubject = models.TextField(db_column='1st Subject')
+    secondSubject = models.TextField(db_column='2nd Subject')
+    thirdSubject = models.TextField(db_column='3rd Subject')
+
+    class Meta:
+        db_table = 'Home Educator Subjects'
+
+class OutsideEducatorSubjects(models.Model):
+    firstSubject = models.TextField(db_column='1st Subject')
+    secondSubject = models.TextField(db_column='2nd Subject')
+    thirdSubject = models.TextField(db_column='3rd Subject')
+
+    class Meta:
+        db_table = 'Outside Educator Subjects'
+
+
+class HomeEducator(models.Model):
+    homeTutorName = models.CharField(max_length=30)
+    homeTutorEmail = models.EmailField(max_length=50)
+    university = models.CharField(max_length=20)
+    department = models.CharField(max_length=20)
+    experience = models.IntegerField()
+    homeTutorLocation = models.CharField(max_length=15)
+    homeTutorContact = models.CharField(max_length=11)
+    homeTutorPassword = models.CharField(max_length=20)
+    homeTutorRating = models.FloatField()
+
+    class Meta:
+         db_table = 'Home Educator Info'
+
+class OutsideEducator(models.Model):
+    outsideTutorName = models.CharField(max_length=30)
+    outsideTutorEmail = models.EmailField(max_length=50)
+    institute = models.CharField(max_length=20)
+    designation = models.CharField(max_length=20)
+    outsideTutorLocation = models.CharField(max_length=15)
+    outsideTutorContact = models.CharField(max_length=11)
+    outsideTutorPassword = models.CharField(max_length=20)
+    outsideTutorRating = models.FloatField()
+
+    class Meta:
+         db_table = 'Outside Educator Info'
+
+
+class TemporaryE(models.Model):
+    id = models.IntegerField(primary_key=True)
+    tutorName = models.TextField(db_column='Tutor Name')
+    tutorPassword = models.CharField(max_length=20,db_column='Password')
+
+    class Meta:
+        db_table = "Educator Temporary Table"
+
